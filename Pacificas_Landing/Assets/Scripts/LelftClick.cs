ef9884ba6d4aed49f10e8773682421268e2c49aa
Laser11 <robertcoughlin11@gmail.com>
2015-11-14 14:19:09 -0800

    Selection
    Rudimentary raycast for mouse clicks, I haven't added it in to any
    specific game object.

---
+++ Pacificas_Landing/Assets/Scripts/LelftClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LelftClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray rayClick = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(rayClick, out hit))
+            { 
+                //activate drop down menu?
+            }
+
+	}
+}
