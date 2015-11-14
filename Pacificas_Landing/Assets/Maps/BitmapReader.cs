c0146087f3e5cce9502187d17e5c8bd945ca88aa
Kamashamasay <tmctigue@mindspring,com>
2015-11-13 20:54:48 -0800

    Map information and broken bitmap reader
    Map information and broken bitmap reader

---
+++ Pacificas_Landing/Assets/Maps/BitmapReader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Object;
+using System.IO.File;
+
+public class BitmapReader
+
+	// Use this for initialization
+	void Start () {
+		string 
+	    Image mapGinger = FromFile(Basic Map);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Pacificas_Landing/Assets/Maps/Map INfo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	/* 
+		blue = rock/mountain
+		red = sand
+		green = grass
+		cyan = hills
+		white = water
+		
+		
+		MIght want to change to make RGB easier.
+		
+	'
+	
+	
+	
+	*/
+}
