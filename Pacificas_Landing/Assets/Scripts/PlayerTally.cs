5ec9c434756c507c7750e0c5190583bb3bc9e45b
htran0109 <htran0109@gmail.com>
2016-01-11 01:47:01 -0800

    Revert "Player Code"
    This reverts commit a5a49b21aa0df2a299f79ef153b34c4cbfc24052.

--- Pacificas_Landing/Assets/Scripts/PlayerTally.cs
+++ Pacificas_Landing/Assets/Scripts/PlayerTally.cs
@@ -1,45 +1,25 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
-
 
 public class PlayerTally : MonoBehaviour {
 
     private int P_Fresh;
     private int P_End;
     private int P_Count;
-
-    public bool Friendly;
-    private string ID_Fresh = "Fresh";
-    private string ID_End = "Finished";
-    private string Label;
-    Text textbox;
-
+  
+    
 	// Use this for initialization
 	void Start () {
-        if (Friendly)
-        {
-            ID_Fresh += "P";
-            ID_End += "P";
-            Label = "Friendly Units: ";
-        }
-        else
-        {
-            ID_Fresh += "E";
-            ID_End += "E";
-            Label = "Enemy Units: ";
-        }
-       textbox = gameObject.GetComponent<Text>();
+
+        P_Fresh = GameObject.FindGameObjectsWithTag("FreshP").Length;
+       P_Count = P_Fresh;
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        P_Fresh = GameObject.FindGameObjectsWithTag(ID_Fresh).Length;
-        P_End = GameObject.FindGameObjectsWithTag(ID_End).Length;
-        P_Count = P_Fresh + P_End;
+        P_Fresh = GameObject.FindGameObjectsWithTag("FreshP").Length;
         P_End = P_Count - P_Fresh;
-        textbox.text = Label + P_Fresh + "/" + P_Count;
-        
-        
+        Display.
 	}
 }
