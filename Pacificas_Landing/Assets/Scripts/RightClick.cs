d291a4183ace22868b3536b909d8bbd33c7ed850
Laser11 <robertcoughlin11@gmail.com>
2016-03-01 19:01:59 -0800

    ?
    I have no idea what has changed.

--- Pacificas_Landing/Assets/Scripts/RightClick.cs
+++ Pacificas_Landing/Assets/Scripts/RightClick.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class RightClick : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -14,7 +15,8 @@ public class RightClick : MonoBehaviour {
         if (Input.GetMouseButtonDown(1))
         {
             this.transform.position = Input.mousePosition;
-
+            
         }
+        
     }
    }
